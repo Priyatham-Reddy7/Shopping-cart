0acf2620142403a547b7274825e15821fd29e3ac
Priyatham Reddy <priyathamreddy147@gmail.com>
2022-07-03 13:18:42 +0530

    Routing setup

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar"
+
+const Home = () => {
+    return (
+        <>
+            <Navbar />
+            <p>Home</p>
+        </>
+    )
+}
+
+export default Home
